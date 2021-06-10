b06f9541664ce7e996bf627c57b4ba9bc557c96e
Mauricio Aznar <mauricioaznar94@gmail.com>
2021-06-10 18:43:13 -0500

    first

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,12 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { property: 'og:title', content: 'Mau Aznar personal portfolio' },
+      {
+        property: 'og:title',
+        content:
+          'Find my app demos, websites, and articles about tech related stuff that I have worked with.',
+      },
+      { property: 'og:url', content: 'https://www.mauaznar.com/' },
       {
         property: 'og:description',
         content:
