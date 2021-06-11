92e6f0460e67d9c87c8245a7a8b8b07e9becee12
Mauricio Aznar <mauricioaznar94@gmail.com>
2021-06-10 22:41:00 -0500

    first

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Mau Aznar',
-    title: 'Mau Aznar',
+    title: 'Personal portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
