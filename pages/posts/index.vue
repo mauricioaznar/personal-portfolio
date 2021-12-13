b9df777b41832364e50392fc7d3c58176a96dec5
mauricioaznar <mauricioaznar94@gmail.com>
2021-12-13 16:13:20 -0600

    tab

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,56 +1,52 @@
 <template>
   <v-container fluid>
-    <v-tabs v-model="tab" class="mb-4">
+    <v-tabs v-model='tab' class='mb-4'>
       <v-tab>Articles</v-tab>
       <v-tab>Snippets</v-tab>
     </v-tabs>
-      <v-tabs-items v-model="tab">
-        <v-tab-item>
-          <v-row justify="center">
-            <v-col sm="12">
-              <div>
-                <h1>Articles</h1>
-                <ul>
-                  <li v-for="article of articles" :key="article.title">
-                    <NuxtLink
-                      :to="{ path: $route.path + article.path }"
-                    >
-                      <div>
-                        <h2>{{ article.title }}</h2>
-                      </div>
-                    </NuxtLink>
-                  </li>
-                </ul>
-              </div>
-            </v-col>
-          </v-row>
-        </v-tab-item>
-        <v-tab-item>
-          <v-row justify="center">
-            <v-col sm="12">
-              <div>
-                <h1>Snippets</h1>
-                <ul>
-                  <li v-for="snippet of snippets" :key="snippet.title">
-                    <NuxtLink
-                      :to="{
+    <v-tabs-items v-model='tab'>
+      <v-tab-item>
+        <v-row justify='start'>
+          <v-col sm='12'>
+            <h1>Articles</h1>
+          </v-col>
+          <v-col v-for='snippet of articles' :key='snippet.title' sm='12' md='6'>
+            <NuxtLink
+              :to='{
+
                   path: $route.path + snippet.path,
-                }"
-                    >
-                      <div>
-                        <h2>{{ snippet.title }}</h2>
-                      </div>
-                    </NuxtLink>
-                  </li>
-                </ul>
-              </div>
-            </v-col>
-          </v-row>
-        </v-tab-item>
-      </v-tabs-items>
+                }'
+              style='text-decoration: none'
+              class='d-flex'
+            >
+              <v-icon>mdi-circle-small</v-icon>
+              <h2>{{ snippet.title }}</h2>
+            </NuxtLink>
+          </v-col>
+        </v-row>
+      </v-tab-item>
+      <v-tab-item>
+        <v-row justify='start'>
+          <v-col sm='12'>
+            <h1>Snippets</h1>
+          </v-col>
+          <v-col v-for='snippet of snippets' :key='snippet.title' sm='12' md='6'>
+            <NuxtLink
+              :to='{
 
+                  path: $route.path + snippet.path,
+                }'
+              style='text-decoration: none'
+              class='d-flex'
+            >
+              <v-icon>mdi-circle-small</v-icon>
+              <h2>{{ snippet.title }}</h2>
+            </NuxtLink>
+          </v-col>
+        </v-row>
+      </v-tab-item>
+    </v-tabs-items>
 
-    </v-row>
   </v-container>
 </template>
 
@@ -60,13 +56,13 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      tab: 0,
+      tab: 0
     }
   },
   watch: {
     tab() {
       window.localStorage.setItem('post_tab', this.tab)
-    },
+    }
   },
   mounted() {
     const postTab = window.localStorage.getItem('post_tab')
@@ -80,19 +76,35 @@ export default Vue.extend({
       } catch (e) {
         ctx.error({
           statusCode: 404,
-          message: 'Message not found',
+          message: 'Message not found'
         })
       }
       return posts
     }
 
+
+    const compare = (a, b) => {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1
+      }
+      if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1
+      }
+      return 0
+    }
+
+
     const articles = await retrievePosts('articles')
     const snippets = await retrievePosts('snippets')
 
     return {
-      articles,
-      snippets,
+      articles: articles.sort(compare),
+      snippets: snippets.sort(compare),
     }
-  },
+  }
 })
 </script>
+
+<style>
+
+</style>
